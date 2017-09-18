d521d2bceb70c95e65cace80c9923dd1399d580c
Juan B Cabral <jbc.develop@gmail.com>
2017-09-17 22:59:13 -0300

    fixes

---
+++ filter2bands.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import tarfile
+
+import pandas as pd
+
+from libs.mppandas import mp_apply
+
+
+# =============================================================================
+# CLASSES
+# =============================================================================
+
+class Filter2Bands(object):
+
+    def __call__(self, df):
+        df["two_bands"] = df.ID.apply(self.extract)
+        return df
+
+    def extract(self, oid):
+        print("Filtering {}...".format(oid))
+        o_path = os.path.join('data', "lc", "{}.tar".format(oid))
+        with tarfile.TarFile(o_path) as tfp:
+            try:
+                tfp.getmember("./{}.I.dat".format(oid))
+                tfp.getmember("./{}.V.dat".format(oid))
+            except:
+                return pd.Series({"two_bands": False})
+        return pd.Series({"two_bands": True})
+
+
+
+
+# =============================================================================
+# FUNCTIONS
+# =============================================================================
+
+def main():
+    if os.path.exists("data/ogle3_2bc.pkl"):
+        return
+    filter2Bands = Filter2Bands()
+    df = pd.read_pickle("data/ogle3.pkl")
+    df2bands = mp_apply(df, filter2Bands)
+    df2bands.to_pickle("data/ogle3_2bc.pkl")
+    df2bands.to_pickle("data/ogle3_2bc.csv")
+
+
+# =============================================================================
+# MAIN
+# =============================================================================
+
+if __name__ == "__main__":
+    main()
