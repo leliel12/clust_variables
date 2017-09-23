fae59c96265da479310f52a0dd0c58fe948732ee
Juan B Cabral <jbc.develop@gmail.com>
2017-09-23 03:25:50 -0300

    foo

--- features.py
+++ features.py
@@ -33,6 +33,11 @@ class Extractor(object):
         sort_mask = time.argsort()
         return time[sort_mask], mag[sort_mask], mag_err[sort_mask]
 
+    def check_dim(self, lc):
+        if lc.ndim == 1:
+            lc.shape = 1, 3
+        return lc
+
     def extract(self, oid):
         print("Extracting {}...".format(oid))
         fs = self._fs
@@ -43,30 +48,23 @@ class Extractor(object):
             i_band = tfp.extractfile(i_member)
             v_band = tfp.extractfile(v_member)
 
-            lc_i = np.loadtxt(i_band)
-            lc_v = np.loadtxt(v_band)
+            lc_i = self.check_dim(np.loadtxt(i_band))
+            lc_v = self.check_dim(np.loadtxt(v_band))
 
-            time_i, mag_i, mag_err_i = self.sort(lc_i[:,0], lc_i[:,1], lc_i[:,2])
-            time_v, mag_v, mag_err_v = self.sort(lc_v[:,0], lc_v[:,1], lc_v[:,2])
+        time_i, mag_i, mag_err_i = self.sort(lc_i[:,0], lc_i[:,1], lc_i[:,2])
+        time_v, mag_v, mag_err_v = self.sort(lc_v[:,0], lc_v[:,1], lc_v[:,2])
 
-            atime, amag, amag2, aerror, aerror2 = feets.preprocess.align(
-                mag_i, mag_v, time_i, time_v, mag_err_i, mag_err_v)
-            import ipdb; ipdb.set_trace()
-            a=1
+        atime, amag, amag2, aerror, aerror2 = feets.preprocess.align(
+            mag_i, mag_v, time_i, time_v, mag_err_i, mag_err_v)
 
+        lc = np.array([
+            mag_i, time_i, mag_err_i, mag_v,
+            amag, amag2, atime, aerror, aerror2])
 
-
-
-        #~ lc = np.loadtxt(lc_path)
-        #~ time, mag, mag_err = lc[:,0], lc[:,1], lc[:,2]
-
-        #~
-
-        #~ with warnings.catch_warnings():
-            #~ warnings.simplefilter("ignore")
-            #~ result = dict(zip(*fs.extract_one(data)))
-
-        #~ return pd.Series(result)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            result = dict(zip(*fs.extract_one(lc)))
+        return pd.Series(result)
 
 
 # =============================================================================
@@ -85,12 +83,13 @@ def main():
     extractor = Extractor(fs)
 
     df = pd.read_pickle("data/ogle3_2bc.pkl")
-    df = df[df.two_bands == True]
-    #~ df = mp_apply(df, filter2Bands)
+    df = df[df.two_bands == True].sample(500)
+    df = mp_apply(df, extractor)
 
-    extractor(df)
+    #~ df = extractor(df)
+    df["DIFF_p"] = np.abs(df.P_1 - df.PeriodLS)
+    print df[["DIFF_p"]].describe()
 
-    #~ features = mp_apply(df, extractor)
     #~ features.to_pickle("features/features.pkl")
     #~ features.to_csv("features/features.csv", index=False)
 
